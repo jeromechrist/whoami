365a804746a7f63c356e09edf3acb29281810126
Jérôme CHRIST <jerome.christ@live.fr>
2018-07-12 22:15:46 +0200

    added PATH_BASE env variable to set base path when routing with a path rule

--- src/Startup.cs
+++ src/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -26,19 +27,29 @@ namespace whoami
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.All;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseHsts();
+                // app.UseHsts();
             }
+
+            if (Environment.GetEnvironmentVariable("PATH_BASE") != null)
+                app.UsePathBase(Environment.GetEnvironmentVariable("PATH_BASE"));
 
             //app.UseHttpsRedirection();
             app.UseMvc();
